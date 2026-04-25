21966f91218d3197349ba01050b50c090fdad2a1
Ankan Ghosh <142795534+ankan00V@users.noreply.github.com>
2026-04-26 00:35:22 +0530

    fix: add components barrel and update App imports to use it

---
+++ components/index.ts
@@ -0,0 +1,57 @@
+import * as _Sidebar from '../Sidebar';
+export * from '../Sidebar';
+export const Sidebar = (_Sidebar as any).Sidebar || (_Sidebar as any).default;
+
+import * as _BotMessage from '../BotMessage';
+export * from '../BotMessage';
+export const BotMessage = (_BotMessage as any).BotMessage || (_BotMessage as any).default;
+
+import * as _TemplateManager from '../TemplateManager';
+export * from '../TemplateManager';
+export const TemplateManager = (_TemplateManager as any).TemplateManager || (_TemplateManager as any).default;
+
+import * as _Login from '../Login';
+export * from '../Login';
+export const Login = (_Login as any).Login || (_Login as any).default;
+
+import * as _LogViewer from '../LogViewer';
+export * from '../LogViewer';
+export const LogViewer = (_LogViewer as any).LogViewer || (_LogViewer as any).default;
+
+import * as _ReportViewer from '../ReportViewer';
+export * from '../ReportViewer';
+export const ReportViewer = (_ReportViewer as any).ReportViewer || (_ReportViewer as any).default;
+
+import * as _DataVisualizer from '../DataVisualizer';
+export * from '../DataVisualizer';
+export const DataVisualizer = (_DataVisualizer as any).DataVisualizer || (_DataVisualizer as any).default;
+
+import * as _ModeIntro from '../ModeIntro';
+export * from '../ModeIntro';
+export const ModeIntro = (_ModeIntro as any).ModeIntro || (_ModeIntro as any).default;
+
+import * as _Toast from '../Toast';
+export * from '../Toast';
+export const ToastContainer = (_Toast as any).ToastContainer || (_Toast as any).default;
+
+import * as _DeleteConfirmDialog from '../DeleteConfirmDialog';
+export * from '../DeleteConfirmDialog';
+export const DeleteConfirmDialog = (_DeleteConfirmDialog as any).DeleteConfirmDialog || (_DeleteConfirmDialog as any).default;
+
+import * as _TourGuide from '../TourGuide';
+export * from '../TourGuide';
+export const TourGuide = (_TourGuide as any).TourGuide || (_TourGuide as any).default;
+
+export default {
+  Sidebar,
+  BotMessage,
+  TemplateManager,
+  Login,
+  LogViewer,
+  ReportViewer,
+  DataVisualizer,
+  ModeIntro,
+  ToastContainer,
+  DeleteConfirmDialog,
+  TourGuide
+};
